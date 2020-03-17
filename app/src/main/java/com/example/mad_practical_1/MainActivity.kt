7970cce28de1122e1c2cecca85b3d27876b070de
XUAN <zhixuan218@gmail.com>
2020-03-17 18:12:15 +0800

    Fifth commit

--- app/src/main/java/com/example/mad_practical_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_1/MainActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.IntegerRes
 import timber.log.Timber
+
+const val KEY_RANDOM_INT = "random_int_key"
+const val KEY_RANDOM_INT_2 = "random_int_2_key"
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage : ImageView
     lateinit var diceImage2 : ImageView
+
+    var randomInt = 0
+    var randomInt2 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image_2)
+
+        if (savedInstanceState != null) {
+            randomInt = savedInstanceState.getInt(KEY_RANDOM_INT, 0)
+            randomInt2 = savedInstanceState.getInt(KEY_RANDOM_INT_2, 0)
+            setDiceImage()
+        }
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -59,25 +72,45 @@ class MainActivity : AppCompatActivity() {
         Timber.i("onRestart Called")
     }
 
-    private fun rollDice() {
-        diceImage.setImageResource(getRandomDiceImage())
-        diceImage2.setImageResource(getRandomDiceImage())
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(KEY_RANDOM_INT, randomInt)
+        outState.putInt(KEY_RANDOM_INT_2, randomInt2)
+
+        Timber.i("onSaveInstanceState Called")
     }
 
-    private fun getRandomDiceImage() : Int {
-        val randomInt = (1..6).random()
+    private fun rollDice() {
+        setRandomInt()
+        setDiceImage()
+    }
 
+    private fun setRandomInt() {
+        randomInt = (1..6).random()
+        randomInt2 = (1..6).random()
+    }
+
+    private fun setDiceImage() {
+        diceImage.setImageResource(getDiceImage(randomInt))
+        diceImage2.setImageResource(getDiceImage(randomInt2))
+    }
+
+    private fun getDiceImage(randomInt : Int) : Int {
         return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
         }
     }
 
     private fun clearDice() {
+        randomInt = 0
+        randomInt2 = 0
         diceImage.setImageResource(R.drawable.empty_dice)
         diceImage2.setImageResource(R.drawable.empty_dice)
     }
